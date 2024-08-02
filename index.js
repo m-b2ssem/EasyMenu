18730a5ed98633222f72ef87823b9938964f4f26
Basem <basemmahdi@gmail.com>
2024-08-02 20:15:14 +0200

    parsing the params to the event to pass them to facebook

--- index.js
+++ index.js
@@ -1124,7 +1124,45 @@ app.get('/track', async (req, res) => {
     event_time: parseInt(cleanedParams.event_time, 10),
     user_data: {
       fbp: cleanedParams.fbp,
-      fbc: cleanedParams.fbc,
+      client_ip_address: clientIp,
+      client_user_agent: req.headers['user-agent'],
+  },
+  };
+  try {
+    const response = await axios.post(url, { data: [event], test_event_code: 'TEST12735' });
+    console.log("response is: ", response.data);
+    res.status(200).send(response.data);
+  } catch (error) {
+    console.log(error.message);
+    console.log(error.response.data);
+    res.status(500).send(error.response.data);
+  }
+});
+
+app.get('/facebook-track', async (req, res) => {
+  const parameters = req.query;
+
+  // Remove additional quotes from parameter values
+  const cleanedParams = {};
+  for (const [key, value] of Object.entries(parameters)) {
+    cleanedParams[key] = value.replace(/^"|"$/g, '');
+  }
+  console.log(req.headers['user-agent']);
+  // Extract IP address
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log("Client IP: ", clientIp);
+  const accessToken = 'EAAnGQW7VNUIBO6p4y4lr0wfikcJ2ftdXjNF1R2ce8Iz3PcwYMtdXtpp71j7yIuAblF3MwM8BbctzV8whZC82uosU3G2p2ZAac33t5IXoetF9UQ9VMRhtI4xzKb5F858CoZCRCoKSjQQIuBY5PYyVJQRAlKDJa7BQzPT3WKLn2nIEIe9HQpVhgHh0OZCe2STYZBQZDZD';
+  const pixelId = '8004482946310463';
+  const url = `https://graph.facebook.com/v11.0/${pixelId}/events?access_token=${accessToken}`;
+  console.log("event name: ", cleanedParams.event_name);
+  const event = {
+    event_name: cleanedParams.event_name,
+    event_id: cleanedParams.event_id,
+    action_source: cleanedParams.action_source,
+    event_source_url: cleanedParams.event_source_url,
+    event_time: parseInt(cleanedParams.event_time, 10),
+    user_data: {
+      fbp: cleanedParams.fbp,
       client_ip_address: clientIp,
       client_user_agent: req.headers['user-agent'],
   },
